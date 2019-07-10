af6cb2bb432b64f06703fa99cd2c4cc8217f273f
John Higgins <johnhiggins@outlook.ie>
2019-07-10 14:26:18 +0100

    crated reducers and added provider and reducers to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
+// Library imports
 import React from "react";
 import ReactDOM from "react-dom";
+// Reducer imports
+import { reducers } from "./reducers";
+// Component imports
 import App from "./components/App";
+import { Provider } from "react-redux";
+// Function imports
+import { createStore } from "redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
